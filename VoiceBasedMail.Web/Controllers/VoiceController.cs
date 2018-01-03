078e388e77305ca4e12503a85d2ba0b2a2cb9c9e
vizio <vizio@DESKTOP-FMLN4D8>
2018-01-04 01:09:38 +0600

    Add project files.

---
+++ VoiceBasedMail.Web/Controllers/VoiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VoiceBasedMail.Web.Controllers
+{
+    public class VoiceController : Controller
+    {
+        // GET: Voice
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VoiceBasedMail.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VoiceBasedMail.Web.MvcApplication" Language="C#" %>
